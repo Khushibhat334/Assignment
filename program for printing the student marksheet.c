d99b96d5610e32f6c743807b149b515b05df5e9b
Khushi Bhat <146862503+Khushibhat334@users.noreply.github.com>
2023-12-09 20:47:29 +0530

    Update program for printing the student marksheet.c

--- program for printing the student marksheet.c
+++ program for printing the student marksheet.c
@@ -3,7 +3,7 @@ void main()
 {
 struct student
 {
-int n1,n2,n3;
+int n1,n2,n3,m1,m2,m3;
 int tot;
 float avg;
 char r;
@@ -31,7 +31,7 @@ printf("subject2:%d\n",s[i].m2);
 printf("subject3:%d\n",s[i].m3);
 printf("Total:%d\n",s[i].tot);
 printf("Average:%f\n",s[i].avg);
-printf("Result:%c"\n,s[i].r)
+printf("Result:%c"\n,s[i].r);
 }
 
 
